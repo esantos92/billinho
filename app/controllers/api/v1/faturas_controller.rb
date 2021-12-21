bef67cc7d6498bfd69916ec13954189ad1f9569e
Everton Santos <everton.santos@redealumni.com>
2021-12-21 13:22:45 -0300

    Correção dos bugs referentes as Datas das Faturas

--- app/controllers/api/v1/faturas_controller.rb
+++ app/controllers/api/v1/faturas_controller.rb
@@ -7,14 +7,11 @@ module Api
                 render json: {status: 'SUCCESS', message:'Faturas carregadas', data:faturas}, status: :ok
             end
 
+            #Listar fatura individualmente
             def show
                 fatura = Fatura.find(params[:id])
                 render json: {status: 'SUCCESS', message:'Fatura carregada', data:fatura}, status: :ok
             end
-
-
-
-
         end
     end
 end
--- app/use_cases/criar_fatura.rb
+++ app/use_cases/criar_fatura.rb
@@ -23,20 +23,20 @@ class CriarFatura
     end
 
     def data_primeira_fatura (dia_vencimento)
-        data_hoje = Date.today.day
-        mes_hoje = Date.today.month
-        ano_hoje = Date.today.year
-        if data_hoje.day.to_i <= dia_vencimento
-            primeiro_vencimento = Date.new(ano_hoje, mes_hoje, dia_vencimento)
-        else
-            mes_hoje = Date.today.next_month.month
-            primeiro_vencimento = Date.new(ano_hoje, mes_hoje, dia_vencimento)
+        dia_hoje = Date.today.day.to_i        
+        if dia_hoje <= dia_vencimento
+            primeiro_vencimento = Date.today.change(day: dia_vencimento)
+        else             
+            primeiro_vencimento = Date.today.next_month.change(day: dia_vencimento)
         end
     end
 
     def gerar_faturas
         data_fatura = data_primeira_fatura(dia_vencimento)
         quantidade_faturas.times do
+            if data_fatura.month.to_i != 2 && data_fatura.day.to_i < dia_vencimento
+                data_fatura = data_fatura.change(day: dia_vencimento)
+            end
             criar_fatura(data_fatura)
             data_fatura = data_fatura.next_month
         end
